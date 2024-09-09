dd0ab1eb398de779f923fb2f3049495b9bcaee34
gunasekhar123117 <gunasekharpasarla@gmail.com>
2024-09-09 19:33:24 +0530

    Add all files

---
+++ script.js
@@ -0,0 +1,27 @@
+ 
+ let card =document.querySelectorAll(".trend")
+ let mainpage = document.querySelector(".main")
+
+ function shops(){
+    mainpage.style.display = "none";
+    blog.style.display =" none";
+   // card.style.display= "none"
+
+ }
+
+ function blog(){
+    mainpage.style.display = "none";
+    card.style.display= "none"
+
+ }
+
+
+
+
+
+
+    
+
+
+ 
+ 
